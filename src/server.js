f661f1b1c553ef4c9e4144c6b25bed4d038da934
mksigrest <mksigrest@gmail.com>
2025-10-04 13:42:11 -0400

    added json
    added route for json file

--- src/server.js
+++ src/server.js
@@ -5,6 +5,7 @@ const fs = require('fs');
 
 const style = path.join(__dirname, '..', 'client', 'client.css');
 const html = path.join(__dirname, '..', 'client', 'client.html');
+const json = path.join(__dirname, '..', 'jsonFile', 'countries.json');
 
 const PORT = process.env.PORT || process.env.NODE_PORT || 3000;
 
