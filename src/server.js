77487d3a51a08408631917dbbd62cb1a664d8a49
mksigrest <mksigrest@gmail.com>
2025-10-04 13:33:25 -0400

    updated
    added PORT and html link to html page

--- src/server.js
+++ src/server.js
@@ -3,3 +3,5 @@ const url = require('url');
 
 const style = path.join(__dirname, '..', 'client', 'client.css');
 const html = path.join(__dirname, '..', 'client', 'client.html');
+
+const PORT = process.env.PORT || process.env.NODE_PORT || 3000;
