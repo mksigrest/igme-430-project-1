4f1a6f7653c2da5637baf4073d17affaef105430
mksigrest <mksigrest@gmail.com>
2025-10-04 13:31:41 -0400

    base paths added
    added base paths for http, url, along with the style and html pages

---
+++ src/server.js
@@ -0,0 +1,5 @@
+const http = require('http');
+const url = require('url');
+
+const style = path.join(__dirname, '..', 'client', 'client.css');
+const html = path.join(__dirname, '..', 'client', 'client.html');
