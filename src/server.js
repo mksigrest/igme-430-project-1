71687244fe7ab3dd74562b60054a8464c13f5ae5
mksigrest <mksigrest@gmail.com>
2025-10-13 20:19:21 -0400

    trying to fix debugging

--- src/server.js
+++ src/server.js
@@ -2,12 +2,7 @@ const http = require('http');
 const url = require('url');
 const path = require('path');
 const fs = require('fs');
-/*
-import http from 'http';
-import url from 'url';
-import path from 'path';
-import fs from 'fs';
-*/
+
 const style = path.join(__dirname, '..', 'client', 'client.css');
 const html = path.join(__dirname, '..', 'client', 'client.html');
 const json = path.join(__dirname, '..', 'jsonFile', 'countries.json');
